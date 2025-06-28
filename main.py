2c398814429e25c80ac8d1f4ad66fa87e41bbf2f
Josh Bonnick <jbonnick@acquiredigital.com>
2025-06-28 20:16:47 +0100

    generate report

--- main.py
+++ main.py
@@ -6,12 +6,28 @@ from stats import *
 def main(book_path):
     content = get_book_content(book_path);    
     word_count = get_word_count(content)
-    character_count = list(get_char_count(content)).sort(reverse=True, key=str.lower)
+    character_count = get_char_count(content)
+    sorted_character_count = get_sorted_char_count(character_count)
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
 
+    for c in sorted_character_count:
+        if(not c["char"].isalpha()):
+            continue
 
-main(sys.argv[1])
+        print(c["char"] + ': ' + str(c["num"]));
+
+    print("============= END ===============")
+
+book_path = None
+
+if(len(sys.argv) > 1):
+    book_path = sys.argv[1]
+else:
+    book_path = 'books/frankenstein.txt'
+
+main(book_path)
